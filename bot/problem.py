d27779296bd89ee8c8db5159db304db38c5d1a4a
SavagePastaMan <69145546+SavagePastaMan@users.noreply.github.com>
2022-08-30 16:54:30 -0400

    Make `is_teacher` a decorator

--- bot/problem.py
+++ bot/problem.py
@@ -6,8 +6,10 @@ from bot.constants import Guild
 
 log = logging.getLogger(__name__)
 
-async def is_teacher(ctx):
-    return ctx.author.id in Guild.teachers
+def is_teacher():
+    async def predicate(ctx):
+        return ctx.author.id in Guild.teachers
+    return commands.check(predicate)
 
 class Problem(commands.Cog):
     def __init__(self, bot):
@@ -19,10 +21,11 @@ class Problem(commands.Cog):
     def setup_database(self):
         cur = self.conn.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS problems(id INTEGER PRIMARY KEY, description TEXT)")
+        cur.execute("CREATE TABLE IF NOT EXISTS test_cases(tc_id INTEGER, test_case TEXT, problem_number INTEGER, FOREIGN KEY(problem_number) REFERENCES problems(id)")
         self.conn.commit()
 
     @commands.command()
-    @commands.check(is_teacher)
+    @is_teacher()
     async def post(self, ctx, *, description: str):
         print(f"{ctx.author} used post with {description=}")
         cur = self.conn.cursor()
@@ -38,11 +41,12 @@ class Problem(commands.Cog):
         await ctx.send(str(data))
     
     @commands.command()
-    @commands.check(is_teacher)
+    @is_teacher()
     async def edit(self, ctx, problem_number: int, *, description: str):
         cur = self.conn.cursor()
         cur.execute("UPDATE problems SET description = ?2 WHERE id = ?1", (problem_number, description))
 
+        log.info("Updated description of %d" % problem_number)
         await ctx.send(f"Successfully updated problem {problem_number}")
 
 async def setup(bot):
